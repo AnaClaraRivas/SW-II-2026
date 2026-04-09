1d8039075764c83368fd70f5b8d8ce6238ae85e8
Ana Clara Bronzeri Rivas <159500946+AnaClaraRivas@users.noreply.github.com>
2026-04-09 07:51:16 -0300

    final

--- AULA05/exercicios/exercicio03.php
+++ AULA05/exercicios/exercicio03.php
@@ -1,25 +1,19 @@
 <?php
 
-    // pegando o json 
     $conteudo = file_get_contents("produtos.json");
 
-    // array
     $dados = json_decode($conteudo, true);
 
-    // criar um novo produto
     $novo_produto = [
-        "nome" => "PC Gamer",
-        "preco" => 6.890,
-        "quantidade" => 34
+        "nome" => "pelucia",
+        "preco" => 567,
+        "quantidade" => 12
     ];
 
-    // adicionando ao array
-    $dados["produtos"][] = $novo_produto;
+    $dados["produtos"][] = $novoproduto;
 
-    // converter de volta ao json
     $json_novo = json_encode($dados);
 
-    // salvar no arquivo
     file_put_contents("produtos.json", $json_novo);
 
 ?>
