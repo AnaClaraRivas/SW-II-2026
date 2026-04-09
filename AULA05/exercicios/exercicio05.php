5db329215eb359ed0306c3c82e49e4bb96990cc5
Ana Clara Bronzeri Rivas <159500946+AnaClaraRivas@users.noreply.github.com>
2026-04-09 08:26:36 -0300

    final

--- AULA05/exercicios/exercicio05.php
+++ AULA05/exercicios/exercicio05.php
@@ -1,28 +1,20 @@
 <?php
 
-    // pegando o json
     $conteudo = file_get_contents("produtos.json");
-
-    // array
     $dados = json_decode($conteudo, true);
-
-    // nome para remover
-    $nome_remover = "PC Gamer";
+    $nomeremov = "Moto";
 
     foreach ($dados["produtos"] as $i => $produto) {
 
-        if ($produto["nome"] == $nome_remover) {
+        if ($produto["nome"] == $nomeremov) {
             unset($dados["produtos"][$i]);
         }
     }
 
-    // organizando
     $dados["produtos"] = array_values($dados["produtos"]);
 
-    // json
-    $json = json_encode($dados);
 
-    // salvar json novo
+    $json = json_encode($dados);
     file_put_contents("produtos.json", $json);
 
 
